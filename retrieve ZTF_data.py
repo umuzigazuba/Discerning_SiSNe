afe235f3e70c83c1bee63cd38839df17b7d18912
umuzigazuba <erin.umuzigazuba@gmail.com>
2024-09-29 18:56:50 +0200

    Requested first batch of ZTF data

--- retrieve ZTF_data.py
+++ retrieve ZTF_data.py
@@ -1,8 +1,6 @@
 # %%
 import subprocess
 
-# %%
-
 # File path to your input text file
 input_file = "ZTF_api/ZTF_info.txt"
 
@@ -14,7 +12,7 @@ userpass = "lvwb246"
 with open(input_file, 'r') as file:
     for line in file:
         # Split the line into components (assumes space-separated values)
-        ra_deg, dec_deg, jd_start, jd_end = line.strip().split()
+        name, ra_deg, dec_deg, jd_start, jd_end = line.strip().split()
         
         # Construct the wget command
         command = f'wget --http-user=ztffps --http-passwd=dontgocrazy! -O log.txt "https://ztfweb.ipac.caltech.edu/cgi-bin/requestForcedPhotometry.cgi?ra={ra_deg}&dec={dec_deg}&jdstart={jd_start}&jdend={jd_end}&email={email}&userpass={userpass}"'
